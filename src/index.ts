43ec1cb9860a43b4d78b6345ea2d530e6c9fec7f
Thagoon <thagoon.work@gmail.com>
2024-08-23 17:15:29 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,54 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import Bun from "bun";
+import CryptoJS from "crypto-js";
+
+declare module "bun" {
+  interface Env {
+    KEY: string;
+    IV: string;
+  }
+}
+
+const encryptFnc = (value: string) => {
+  const key = CryptoJS.enc.Utf8.parse(Bun.env.KEY);
+  const iv = CryptoJS.enc.Utf8.parse(Bun.env.IV);
+  const encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(value.toString()), key, {
+    keySize: 128 / 8,
+    iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+  });
+
+  return encrypted.toString();
+};
+
+const decryptFnc = (value: string) => {
+  const key = CryptoJS.enc.Utf8.parse("b75524255a7f54d2726a951bb39204df");
+  const iv = CryptoJS.enc.Utf8.parse("1583288699248111");
+  const decrypted = CryptoJS.AES.decrypt(value, key, {
+    keySize: 128 / 8,
+    iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+  });
+
+  return JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
+};
 
 const port = process.env.PORT || 3000;
-const app = new Elysia().get("/", () => "Hello Elysia").listen(port);
+const app = new Elysia().get("/", () => "Hello Elysia")
+  .post(
+    "/",
+    ({ body }) => {
+      const r = decryptFnc(body.data);
+      return r;
+    },
+    {
+      body: t.Object({
+        data: t.String(),
+      }),
+    }
+  ).listen(port);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
